66e6f2ca074ef9413a955f8b600e68259243c8c4
Abas Muzi <106030034+AbasPortfolio@users.noreply.github.com>
2025-04-25 10:30:21 +0200

    Update axios.js

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "https://backend-j6o1.onrender.com/api",
+  baseURL: "https://backend-j6o1.onrender.com",
   withCredentials: true,
 });
